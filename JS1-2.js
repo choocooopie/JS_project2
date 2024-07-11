c8b2b9fe116dd30b9a843fe09a4a75b1e006b3be
Alex Oh <odange2119@gmail.com>
2024-07-11 23:38:39 +0900

    first commit4

--- JS1-2.js
+++ JS1-2.js
@@ -1,65 +1,114 @@
-let taskinput = document.getElementById("task-input")
-let addbutton = document.getElementById("add-button")
-let tabs = document.querySelectorAll("task-taks div")
-let tasklist = []
-addbutton.addEventListener("click", addtask)
+let userInput = document.querySelector(".task-input");
+let addButton = document.querySelector(".button-add");
+let tabs = document.querySelectorAll(".tab-type div");
+let underLine = document.getElementById("tab-underline");
+let taskList = [];
+let mode = "all";
+let filterList = [];
 
-function addtask() {
-    let task = {
-        id:randomidgenerate(),
-        taskcontent: taskinput.value,
-        iscomplete:false,
-    }
-    tasklist.push(task)
-    console.log(tasklist)
-    render()
+addButton.addEventListener("mousedown", addTask);
+userInput.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    addTask(event);
+  }
+});
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
+
+function addTask() {
+  let taskValue = userInput.value;
+  if (taskValue === "") return alert("할일을 입력해주세요");
+  let task = {
+    content: taskValue,
+    isComplete: false,
+    id: randomIDGenerator(),
+  };
+
+  taskList.push(task);
+  userInput.value = "";
+  render();
 }
 
 function render() {
-    let resultHTML = ""
-    for(let i=0;i<tasklist.length;i++) {
-        if(tasklist[i].iscomplete == true) {
-            resultHTML += `<div class="task">
-                    <div class="task-done">${tasklist[i].taskcontent}</div>
-                    <div>
-                        <button onclick="togglecomplete('${tasklist[i].id}')">Check</button>
-                        <button onclick="deletetask('${tasklist[i].id}')">Delete</button>
-                    </div>
-                </div>`
-        }else {
-            resultHTML += `<div class="task">
-                    <div>${tasklist[i].taskcontent}</div>
-                    <div>
-                        <button onclick="togglecomplete('${tasklist[i].id}')">Check</button>
-                        <button onclick="deletetask('${tasklist[i].id}')">Delete</button>
-                    </div>
-                </div>`
-        }
+  let result = "";
+  list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else {
+    list = filterList;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete) {
+      result += `<div class="task task-done" id="${list[i].id}">
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
+    } else {
+      result += `<div class="task" id="${list[i].id}" >
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
     }
+  }
 
-
-    document.getElementById("task-board").innerHTML = resultHTML
+  document.getElementById("task-board").innerHTML = result;
 }
 
-function togglecomplete(id) {
-    for(let i=0;i<tasklist.length;i++)
-        if(tasklist[i].id == id){
-            tasklist[i].iscomplete = !tasklist[i].iscomplete
-            break
-        }
-    render()
-}
-
-function deletetask(id) {
-    for(let i=0;i<tasklist.length;i++) {
-        if(tasklist[i].id == id) {
-            tasklist.splice(i,1)
-            break
-        }
+function toggleDone(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
     }
-    render()
+  }
+  filter();
 }
 
-function randomidgenerate() {
-    return '_' + Math.random().toString(36).substr(2, 9);
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i, 1);
+    }
+  }
+
+  filter();
 }
+function filter(e) {
+  if (e) {
+    mode = e.target.id;
+    underLine.style.width = e.target.offsetWidth + "px";
+    underLine.style.left = e.target.offsetLeft + "px";
+    underLine.style.top =
+      e.target.offsetTop + (e.target.offsetHeight - 4) + "px";
+
+  filterList = [];
+  if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
+    }
+  }
+  render();
+}
+
+function randomIDGenerator() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
+
